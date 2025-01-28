f8789939cfa388f553a5327c077c19098d04a2a3
Antoni Czaplicki <antekczaplicki@gmail.com>
2025-01-28 13:56:45 +0100

    fix: show what invalid property are invalid in validation

--- src/components/quiz/helpers/quizValidation.ts
+++ src/components/quiz/helpers/quizValidation.ts
@@ -4,7 +4,7 @@ import {Quiz} from "../types.ts";
 // Allowed keys for each object type
 const ALLOWED_QUIZ_KEYS = [
     "title", "description", "visibility", "allow_anonymous",
-    "is_anonymous", "version", "questions"
+    "is_anonymous", "version", "questions", "maintainer"
 ];
 const ALLOWED_QUESTION_KEYS = [
     "id", "question", "explanation", "multiple", "image", "answers"
@@ -14,7 +14,8 @@ const ALLOWED_ANSWER_KEYS = ["answer", "correct", "image"];
 export const validateQuiz = (quiz: Quiz): string | null => {
     // Check if quiz contains only allowed properties
     if (!containsOnlyAllowedKeys(quiz, ALLOWED_QUIZ_KEYS)) {
-        return "Quiz zawiera nieprawidłowe właściwości.";
+        const invalidKeys = Object.keys(quiz).filter((key) => !ALLOWED_QUIZ_KEYS.includes(key));
+        return "Quiz zawiera nieprawidłowe właściwości: " + invalidKeys.join(", ");
     }
 
     if (!quiz.title.trim()) {
@@ -28,7 +29,8 @@ export const validateQuiz = (quiz: Quiz): string | null => {
     for (const [questionIndex, question] of quiz.questions.entries()) {
         // Check if question contains only allowed properties
         if (!containsOnlyAllowedKeys(question, ALLOWED_QUESTION_KEYS)) {
-            return `Pytanie nr ${questionIndex + 1} zawiera nieprawidłowe właściwości: ${Object.keys(question).join(", ")}.`;
+            const invalidKeys = Object.keys(question).filter((key) => !ALLOWED_QUESTION_KEYS.includes(key));
+            return `Pytanie nr ${questionIndex + 1} zawiera nieprawidłowe właściwości: ${invalidKeys.join(", ")}`;
         }
 
         if (!question.question.trim()) {
@@ -42,7 +44,8 @@ export const validateQuiz = (quiz: Quiz): string | null => {
         for (const [answerIndex, answer] of question.answers.entries()) {
             // Check if answer contains only allowed properties
             if (!containsOnlyAllowedKeys(answer, ALLOWED_ANSWER_KEYS)) {
-                return `Odpowiedź nr ${answerIndex + 1} w pytaniu nr ${questionIndex + 1} zawiera nieprawidłowe właściwości.`;
+                const invalidKeys = Object.keys(answer).filter((key) => !ALLOWED_ANSWER_KEYS.includes(key));
+                return `Odpowiedź nr ${answerIndex + 1} w pytaniu nr ${questionIndex + 1} zawiera nieprawidłowe właściwości: ${invalidKeys.join(", ")}`;
             }
 
             if (!answer.answer.trim()) {
