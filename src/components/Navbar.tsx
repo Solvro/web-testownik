f3e313f58fe38a3450c8b53f5739bd3dfcd6d71c
Kamil Marczak <79667721+qamarq@users.noreply.github.com>
2025-02-05 18:42:46 +0100

    feat: feedback form (#50)
    * feat: modal base
    
    * feat: complete form
    
    * fix: better error handling
    
    * feat: update form
    
    * fix: update validation and fiz typo
    
    ---------
    
    Co-authored-by: Antoni Czaplicki <antekczaplicki@gmail.com>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,22 @@
-import React, {useCallback, useContext, useEffect, useMemo, useState} from 'react';
-import {Navbar as BSNavbar, Nav, Container, Button, Badge} from 'react-bootstrap';
-import {Icon} from "@iconify/react";
-import AppContext from "../AppContext.tsx";
-import {SERVER_URL} from "../config.ts";
-import {useLocation, useNavigate} from "react-router";
+import React, {
+    useCallback,
+    useContext,
+    useEffect,
+    useMemo,
+    useState,
+} from 'react';
+import {
+    Navbar as BSNavbar,
+    Nav,
+    Container,
+    Button,
+    Badge,
+} from 'react-bootstrap';
+import { Icon } from '@iconify/react';
+import AppContext from '../AppContext.tsx';
+import { SERVER_URL } from '../config.ts';
+import { useLocation, useNavigate } from 'react-router';
+import ReportBugModal from './ReportBugModal.tsx';
 
 const Navbar: React.FC = () => {
     const appContext = useContext(AppContext);
@@ -11,31 +24,35 @@ const Navbar: React.FC = () => {
     const location = useLocation();
 
     const [expanded, setExpanded] = useState(false);
+    const [showReportModal, setShowReportModal] = useState(false);
 
-    const queryParams = useMemo(() => new URLSearchParams(location.search), [location.search]);
+    const queryParams = useMemo(
+        () => new URLSearchParams(location.search),
+        [location.search]
+    );
     const accessToken = queryParams.get('access_token');
     const refreshToken = queryParams.get('refresh_token');
 
     const fetchUserData = useCallback(async () => {
         try {
-            const response = await appContext.axiosInstance.get("/user/");
+            const response = await appContext.axiosInstance.get('/user/');
             if (!response.data) {
-                throw new Error("No user data available");
+                throw new Error('No user data available');
             }
             const userData = response.data;
-            localStorage.setItem("profile_picture", userData.photo);
-            localStorage.setItem("is_staff", userData.is_staff);
-            localStorage.setItem("user_id", userData.id);
+            localStorage.setItem('profile_picture', userData.photo);
+            localStorage.setItem('is_staff', userData.is_staff);
+            localStorage.setItem('user_id', userData.id);
             appContext.setAuthenticated(true);
         } catch {
-            console.error("Failed to fetch user data");
+            console.error('Failed to fetch user data');
         }
     }, [appContext]);
 
     const handleLogin = useCallback(async () => {
         if (accessToken && refreshToken) {
-            localStorage.setItem("access_token", accessToken);
-            localStorage.setItem("refresh_token", refreshToken);
+            localStorage.setItem('access_token', accessToken);
+            localStorage.setItem('refresh_token', refreshToken);
 
             queryParams.delete('access_token');
             queryParams.delete('refresh_token');
@@ -53,19 +70,22 @@ const Navbar: React.FC = () => {
     }, [handleLogin]);
 
     const handleLogout = () => {
-        localStorage.removeItem("access_token");
-        localStorage.removeItem("refresh_token");
-        localStorage.removeItem("profile_picture");
-        localStorage.removeItem("is_staff");
-        localStorage.removeItem("user_id");
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('refresh_token');
+        localStorage.removeItem('profile_picture');
+        localStorage.removeItem('is_staff');
+        localStorage.removeItem('user_id');
         appContext.setAuthenticated(false);
-        navigate("/");
+        navigate('/');
     };
 
-    const handleNavigation = (path: string, event: React.MouseEvent<HTMLElement>) => {
+    const handleNavigation = (
+        path: string,
+        event: React.MouseEvent<HTMLElement>
+    ) => {
         if (event.metaKey || event.ctrlKey) {
             // Open in a new tab
-            window.open(path, "_blank");
+            window.open(path, '_blank');
         } else {
             // Navigate normally
             navigate(path);
@@ -80,9 +100,8 @@ const Navbar: React.FC = () => {
             <Container fluid>
                 <Nav.Link
                     onClick={(event) => {
-                        handleNavigation("/", event);
-                    }}
-                >
+                        handleNavigation('/', event);
+                    }}>
                     <BSNavbar.Brand>
                         <img
                             src="/solvro_mono.svg"
@@ -92,27 +111,34 @@ const Navbar: React.FC = () => {
                         />
                     </BSNavbar.Brand>
                 </Nav.Link>
-                <BSNavbar.Toggle aria-controls="navbarNav" onClick={() => setExpanded(!expanded)}/>
+                <BSNavbar.Toggle
+                    aria-controls="navbarNav"
+                    onClick={() => setExpanded(!expanded)}
+                />
                 <BSNavbar.Collapse id="navbarNav">
                     <Nav className="me-auto mb-2 mb-lg-0">
                         <Nav.Link
-                            onClick={(event) => handleNavigation("/quizzes", event)}
-                            active={isActive("/quizzes")}
-                        >
+                            onClick={(event) =>
+                                handleNavigation('/quizzes', event)
+                            }
+                            active={isActive('/quizzes')}>
                             Twoje quizy
                         </Nav.Link>
                         <Nav.Link
-                            onClick={(event) => handleNavigation("/grades", event)}
-                            active={isActive("/grades")}
-                        >
+                            onClick={(event) =>
+                                handleNavigation('/grades', event)
+                            }
+                            active={isActive('/grades')}>
                             Oceny
                         </Nav.Link>
-                        {localStorage.getItem("is_staff") === "true" && (
+                        <Nav.Link onClick={() => setShowReportModal(true)}>
+                            Zgłoś błąd
+                        </Nav.Link>
+                        {localStorage.getItem('is_staff') === 'true' && (
                             <Nav.Link
                                 href={`${SERVER_URL}/admin/`}
                                 target="_blank"
-                                active={false}
-                            >
+                                active={false}>
                                 Panel administratora
                             </Nav.Link>
                         )}
@@ -121,25 +147,31 @@ const Navbar: React.FC = () => {
                         {appContext.isGuest ? (
                             <>
                                 <Nav.Link
-                                    onClick={(event) => handleNavigation("/profile", event)}
-                                >
+                                    onClick={(event) =>
+                                        handleNavigation('/profile', event)
+                                    }>
                                     <Button
                                         variant={appContext.theme.getOppositeTheme()}
-                                        className="d-inline-flex gap-1 align-items-center"
-                                    >
+                                        className="d-inline-flex gap-1 align-items-center">
                                         <Icon icon="fluent:guest-24-filled"></Icon>
                                         <span>Gość</span>
-                                        <Badge bg={appContext.theme.getTheme()}
-                                               style={{color: appContext.theme.getOppositeThemeColor()}}>
+                                        <Badge
+                                            bg={appContext.theme.getTheme()}
+                                            style={{
+                                                color: appContext.theme.getOppositeThemeColor(),
+                                            }}>
                                             <b>BETA</b>
                                         </Badge>
                                     </Button>
                                 </Nav.Link>
-                                <Nav.Link className="ps-0" onClick={() => navigate("/connect-account")}>
+                                <Nav.Link
+                                    className="ps-0"
+                                    onClick={() =>
+                                        navigate('/connect-account')
+                                    }>
                                     <Button
                                         variant="success"
-                                        className="d-flex align-items-center justify-content-center p-2 fs-5"
-                                    >
+                                        className="d-flex align-items-center justify-content-center p-2 fs-5">
                                         <Icon icon="material-symbols:cloud-upload"></Icon>
                                     </Button>
                                 </Nav.Link>
@@ -147,22 +179,28 @@ const Navbar: React.FC = () => {
                         ) : appContext.isAuthenticated ? (
                             <>
                                 <Nav.Link
-                                    onClick={(event) => handleNavigation("/profile", event)}
-                                >
+                                    onClick={(event) =>
+                                        handleNavigation('/profile', event)
+                                    }>
                                     <Button
                                         variant={appContext.theme.getOppositeTheme()}
-                                        className="d-inline-flex gap-1 align-items-center"
-                                    >
-                                        {localStorage.getItem("profile_picture") ? (
+                                        className="d-inline-flex gap-1 align-items-center">
+                                        {localStorage.getItem(
+                                            'profile_picture'
+                                        ) ? (
                                             <img
-                                                src={localStorage.getItem("profile_picture")!}
+                                                src={
+                                                    localStorage.getItem(
+                                                        'profile_picture'
+                                                    )!
+                                                }
                                                 alt="Profilowe"
                                                 id="profile-pic"
                                                 style={{
-                                                    borderRadius: "50%",
-                                                    width: "1.5em",
-                                                    height: "1.5em",
-                                                    objectFit: "cover",
+                                                    borderRadius: '50%',
+                                                    width: '1.5em',
+                                                    height: '1.5em',
+                                                    objectFit: 'cover',
                                                 }}
                                             />
                                         ) : (
@@ -171,23 +209,22 @@ const Navbar: React.FC = () => {
                                         <span>Profil</span>
                                     </Button>
                                 </Nav.Link>
-                                <Nav.Link className="ps-0" onClick={handleLogout}>
+                                <Nav.Link
+                                    className="ps-0"
+                                    onClick={handleLogout}>
                                     <Button
                                         variant="danger"
-                                        className="d-flex align-items-center justify-content-center p-2 fs-5"
-                                    >
+                                        className="d-flex align-items-center justify-content-center p-2 fs-5">
                                         <Icon icon="bi:box-arrow-right"></Icon>
                                     </Button>
                                 </Nav.Link>
                             </>
                         ) : (
                             <Nav.Link
-                                href={`${SERVER_URL}/login/usos?jwt=true&redirect=${document.location}`}
-                            >
+                                href={`${SERVER_URL}/login/usos?jwt=true&redirect=${document.location}`}>
                                 <Button
                                     variant={appContext.theme.getOppositeTheme()}
-                                    className="d-inline-flex gap-1 align-items-center"
-                                >
+                                    className="d-inline-flex gap-1 align-items-center">
                                     <Icon icon="bi:box-arrow-in-right"></Icon>
                                     Zaloguj się
                                 </Button>
@@ -195,6 +232,11 @@ const Navbar: React.FC = () => {
                         )}
                     </Nav>
                 </BSNavbar.Collapse>
+
+                <ReportBugModal
+                    show={showReportModal}
+                    onHide={() => setShowReportModal(false)}
+                />
             </Container>
         </BSNavbar>
     );
---
+++ src/components/ReportBugModal.tsx
@@ -0,0 +1,212 @@
+import React, {useContext, useState} from 'react';
+import {Modal, Button, Form, Row, Col} from 'react-bootstrap';
+import AppContext from '../AppContext.tsx';
+import axios from 'axios';
+import {SERVER_URL} from '../config.ts';
+import {toast} from "react-toastify";
+
+interface ReportBugModalProps {
+    show: boolean;
+    onHide: () => void;
+}
+
+const DEFAULT_FORM_STATE = {
+    name: '',
+    email: '',
+    title: '',
+    content: '',
+    sendDiagnostics: false,
+    diagnostic: '',
+    reportType: 'bug',
+};
+
+const ReportBugModal: React.FC<ReportBugModalProps> = ({show, onHide}) => {
+    const [form, setForm] = useState(DEFAULT_FORM_STATE);
+    const [formErrors, setFormErrors] = useState<Record<string, string>>({});
+    const [isSending, setIsSending] = useState(false);
+
+    const validateForm = () => {
+        const errors: Record<string, string> = {};
+
+        if (!form.name.trim()) errors.name = 'Podaj swoje imię.';
+        if (!form.title.trim()) errors.title = 'Podaj tytuł zgłoszenia.';
+        if (!form.content.trim()) errors.content = 'Podaj treść zgłoszenia.';
+
+        setFormErrors(errors);
+        return Object.keys(errors).length === 0;
+    };
+
+    const handleChange = (
+        e: React.ChangeEvent<
+            HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+        >
+    ) => {
+        const {id, value, type, checked} = e.target as HTMLInputElement;
+        setForm((prev) => ({
+            ...prev,
+            [id]: type === 'checkbox' ? checked : value,
+        }));
+
+        // Clear the error message for this field when user types
+        setFormErrors((prev) => ({
+            ...prev,
+            [id]: '',
+        }));
+    };
+
+    const handleSend = () => {
+        setIsSending(true);
+
+        if (!validateForm()) {
+            setIsSending(false);
+            return;
+        }
+
+        if (form.sendDiagnostics) {
+            const quizId = location.pathname.includes('quiz') ? location.pathname.split('/').pop() : null;
+            const diagnostics = {
+                userAgent: navigator.userAgent,
+                platform: navigator.platform,
+                vendor: navigator.vendor,
+
+                innerWidth: window.innerWidth,
+                innerHeight: window.innerHeight,
+                outerWidth: window.outerWidth,
+                outerHeight: window.outerHeight,
+
+                quiz_id: quizId,
+
+                location: window.location.href,
+                localStorage: {
+                    user_id: localStorage.getItem('user_id'),
+                    is_guest: localStorage.getItem('is_guest'),
+                    is_authenticated: localStorage.getItem('access_token') ? 'true' : 'false',
+                    quiz_progress: quizId ? localStorage.getItem(`${quizId}_progress`) : null,
+                },
+                sessionStorage: JSON.stringify(sessionStorage),
+            };
+            form.diagnostic = JSON.stringify(diagnostics, null, 2);
+        }
+
+        axios
+            .post(`${SERVER_URL}/feedback/send`, {
+                ...form,
+                sendDiagnostics: form.sendDiagnostics ? 'true' : 'false',
+            })
+            .then(() => {
+                setForm(DEFAULT_FORM_STATE);
+                onHide();
+                toast.success('Dziękujemy za zgłoszenie!');
+            })
+            .catch((error) => {
+                toast.error('Wystąpił błąd podczas wysyłania zgłoszenia!', {
+                    position: 'top-center',
+                });
+                console.error(error);
+            })
+            .finally(() => {
+                setIsSending(false);
+            });
+    };
+
+    const appContext = useContext(AppContext);
+
+    return (
+        <Modal show={show} onHide={onHide}>
+            <Modal.Header closeButton>
+                <Modal.Title>Zgłoszenie błędu lub sugestia</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="name">
+                        <Form.Label>Twoja nazwa</Form.Label>
+                        <Form.Control
+                            disabled={isSending}
+                            placeholder="Jan Kowalski"
+                            value={form.name}
+                            onChange={handleChange}
+                            isInvalid={!!formErrors.name}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            {formErrors.name}
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="email">
+                        <Form.Label>Adres e-mail (opcjonalnie)</Form.Label>
+                        <Form.Control
+                            disabled={isSending}
+                            placeholder="jan.kowalski@solvro.pl"
+                            value={form.email}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
+                </Row>
+                <Form.Group className="mb-3" controlId="title">
+                    <Form.Label>Tytuł</Form.Label>
+                    <Form.Control
+                        disabled={isSending}
+                        placeholder="Tytuł zgłoszenia"
+                        value={form.title}
+                        onChange={handleChange}
+                        isInvalid={!!formErrors.title}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {formErrors.title}
+                    </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="content">
+                    <Form.Label>Treść</Form.Label>
+                    <Form.Control
+                        disabled={isSending}
+                        placeholder="Treść zgłoszenia"
+                        as="textarea"
+                        value={form.content}
+                        onChange={handleChange}
+                        isInvalid={!!formErrors.content}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {formErrors.content}
+                    </Form.Control.Feedback>
+                </Form.Group>
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="sendDiagnostics">
+                        <Form.Check
+                            type="checkbox"
+                            label="Wyślij dane diagnostyczne"
+                            checked={form.sendDiagnostics}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="reportType">
+                        <Form.Label>Typ zgłoszenia</Form.Label>
+                        <Form.Select
+                            value={form.reportType}
+                            onChange={handleChange}
+                        >
+                            <option value="bug">Błąd</option>
+                            <option value="enhancement">Propozycja</option>
+                            <option value="question">Pytanie</option>
+                        </Form.Select>
+                    </Form.Group>
+                </Row>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button
+                    variant={`outline-${appContext.theme.getOppositeTheme()}`}
+                    onClick={onHide}
+                >
+                    Anuluj
+                </Button>
+                <Button
+                    disabled={isSending}
+                    variant="primary"
+                    onClick={handleSend}
+                >
+                    Wyślij formularz
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+export default ReportBugModal;
