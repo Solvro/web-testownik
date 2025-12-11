e5f2e766d83c688d6f21b44941cefda33bda87cf
olios123 <oliwier4444k@gmail.com>
2025-12-11 14:01:17 +0100

    refactor: dialog copy

--- src/pages/import-quiz-page.tsx
+++ src/pages/import-quiz-page.tsx
@@ -199,7 +199,10 @@ export function ImportQuizPage(): React.JSX.Element {
 
     for (const child of copyTextElement.children) {
       if (child.nodeName === "DIV") {
-        copyText += `${child.children[1].textContent}\n\n`;
+        const pre = child.querySelector("pre");
+        if (pre?.textContent != null) {
+          copyText += `${pre.textContent}\n\n`;
+        }
         continue;
       }
       copyText += `${child.textContent}\n\n`;
