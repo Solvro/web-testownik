ee3b9a8b3e61a70819d3304dc9e388a406d36fd5
olios123 <oliwier4444k@gmail.com>
2025-12-13 11:00:41 +0100

    refactor: remove comment

--- src/pages/import-quiz-page.tsx
+++ src/pages/import-quiz-page.tsx
@@ -80,7 +80,6 @@ export function ImportQuizPage(): React.JSX.Element {
     handleImport,
   } = useImportQuiz();
 
-  // const appContext = useContext(AppContext);
   const navigate = useNavigate();
   const location = useLocation();
 
