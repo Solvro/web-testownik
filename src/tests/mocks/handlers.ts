f6ac5a1a393e9bc5a35c463bb1f1dd79daa010ad
ShadowCatP <282276@student.pwr.edu.pl>
2025-12-17 13:21:12 +0100

    fix: fixed lint errors after rebase

--- src/tests/mocks/handlers.ts
+++ src/tests/mocks/handlers.ts
@@ -1,10 +1,12 @@
 import { HttpResponse, http } from "msw";
-import { Quiz } from "../../components/quiz/types";
-import { mockTerms, mockCourses } from "./GradesMock";
-import { mockQuiz } from "./QuizMock";
+
+import type { Quiz } from "@/types/quiz";
+
+import { mockCourses, mockTerms } from "./grade-mock";
+import { mockQuiz } from "./quiz-mock";
 
 export const handlers = [
-  http.post("/quizzes/", async ({ request }) => {
+  http.post("*/quizzes/", async ({ request }) => {
     const quiz = (await request.json()) as Quiz;
 
     return HttpResponse.json(
@@ -12,21 +14,21 @@ export const handlers = [
         ...quiz,
         id: "123",
       },
-      { status: 201 }
+      { status: 201 },
     );
   }),
-  http.get("/study-groups/", () => {
+  http.get("*/study-groups/", () => {
     return HttpResponse.json([]);
   }),
-  http.get("/shared-quizzes/", ({ request }) => {
+  http.get("*/shared-quizzes/", ({ request }) => {
     const url = new URL(request.url);
     const quizId = url.searchParams.get("quiz");
     return HttpResponse.json({ quizId, shared: false });
   }),
-  http.get("/grades/", () =>
-    HttpResponse.json({ terms: mockTerms, courses: mockCourses })
+  http.get("*/grades/", () =>
+    HttpResponse.json({ terms: mockTerms, courses: mockCourses }),
   ),
-  http.post("/import-quiz-from-link/", async () =>
-    HttpResponse.json(mockQuiz, { status: 201 })
+  http.post("*/import-quiz-from-link/", () =>
+    HttpResponse.json(mockQuiz, { status: 201 }),
   ),
 ];
