90548e4c6dfb6a845dac3fea016d4042a85af704
Antoni Czaplicki <antekczaplicki@gmail.com>
2026-01-24 07:31:33 +0100

    feat: wrap LoginOTPPageClient in Suspense for improved loading experience

--- src/app/login-otp/page.tsx
+++ src/app/login-otp/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Suspense } from "react";
 
 import { LoginOTPPageClient } from "./client";
 
@@ -7,5 +8,9 @@ export const metadata: Metadata = {
 };
 
 export default function LoginOTPPage() {
-  return <LoginOTPPageClient />;
+  return (
+    <Suspense>
+      <LoginOTPPageClient />
+    </Suspense>
+  );
 }
