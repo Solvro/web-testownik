3a6adee46ea39f4523b968e66cf8f0c4b84cb6a3
Antoni Czaplicki <antekczaplicki@gmail.com>
2026-02-21 22:26:41 +0100

    fix: logo width for Amelka's ipad

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -7,11 +7,11 @@ import Logo from "@/assets/logo.svg";
 export function AppLogo(): React.JSX.Element {
   return (
     <>
-      <Image src={Logo} alt="Logo" className="h-9 w-full dark:hidden" />
+      <Image src={Logo} alt="Logo" className="h-9 w-fit dark:hidden" />
       <Image
         src={LogoDark}
         alt="Logo"
-        className="hidden h-9 w-full dark:block"
+        className="hidden h-9 w-fit dark:block"
       />
     </>
   );
