8394a083aea328f1b06a32879bb06e3e3ff03ed1
olios123 <oliwier4444k@gmail.com>
2026-03-04 13:10:36 +0100

    refactor: remove unused imports

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Checkbox as CheckboxPrimitive } from "@base-ui/react/checkbox";
-import { Check, CheckIcon } from "lucide-react";
+import { Check } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 
