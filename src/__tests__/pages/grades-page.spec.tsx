ba41c0b26d73376aa8a23735ef3301a72493fb34
olios123 <oliwier4444k@gmail.com>
2026-03-05 17:47:30 +0100

    refactor: update tests

--- src/__tests__/pages/grades-page.spec.tsx
+++ src/__tests__/pages/grades-page.spec.tsx
@@ -64,7 +64,7 @@ describe("GradesPage", () => {
     server.use(
       http.get("*/grades/", async () => {
         await delay(200);
-        return HttpResponse.json({});
+        return HttpResponse.json({ terms: [], courses: [] });
       }),
     );
     await setup();
@@ -177,7 +177,9 @@ describe("GradesPage", () => {
 
     const termSelect = screen.getByRole("combobox");
     await user.click(termSelect);
-    await user.click(screen.getByRole("option", { name: mockTerms[0].name }));
+    await user.click(
+      await screen.findByRole("option", { name: mockTerms[0].name }),
+    );
 
     expect(await screen.findByText(mockCourses[0].course_name)).toBeVisible();
     expect(await screen.findByText(mockCourses[1].course_name)).toBeVisible();
