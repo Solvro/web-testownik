01eccd7fac965e2f942f5e8140d54677131b9612
olios123 <oliwier4444k@gmail.com>
2026-05-02 21:26:05 +0200

    chore: change next config

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,6 @@ const nextConfig: NextConfig = {
   experimental: {
     viewTransition: true,
   },
-  trailingSlash: true,
   images: {
     dangerouslyAllowLocalIP: process.env.NODE_ENV === "development",
     remotePatterns: [
